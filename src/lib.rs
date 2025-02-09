c6858109f89008920f78c6c225a29051c81b7201
Konippi <konippi-114@outlook.jp>
2025-02-09 11:44:43 +0900

    fix: hash & refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
 use block::Block;
 use chrono::Utc;
-use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 mod block;
 mod web;
 
 #[wasm_bindgen]
-pub fn mine() {
+pub fn mine() -> Result<(), JsValue> {
     let mut block = Block::new(Utc::now().timestamp(), vec![], &Block::first())
         .expect("failed to create block");
-    block.mine("aa").expect("failed to mine block");
+    block
+        .mine("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
+        .expect("failed to mine block");
     web::add_heading_block(
         block.nonce.to_string(),
         block.hash().expect("failed to hash block"),
-    );
+    )?;
+
+    Ok(())
 }
--- src/web.rs
+++ src/web.rs
@@ -1,8 +1,12 @@
-pub fn add_heading_block(nonce: String, hash: String) {
+use wasm_bindgen::JsValue;
+
+pub fn add_heading_block(nonce: String, hash: String) -> Result<(), JsValue> {
     let window = web_sys::window().expect("no window found");
     let document = window.document().expect("no window found");
     let body = document.body().expect("no window found");
-    let heading = document.create_element("h1").expect("no window found");
+    let heading = document.create_element("p")?;
     heading.set_inner_html(&format!("Nonce: {}, Hash: {}", nonce, hash));
-    body.append_child(&heading).unwrap();
+    body.append_child(&heading)?;
+
+    Ok(())
 }
